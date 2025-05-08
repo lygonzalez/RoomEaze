484a9839e2c80dfba01b79f5a3a6ce15dcee148d
Laura <lygg0589@gmail.com>
2025-05-07 18:44:47 -0700

    navbar

--- RoomEaze/src/pages/navbar/navbar.jsx
+++ RoomEaze/src/pages/navbar/navbar.jsx
@@ -4,7 +4,7 @@ import "./Navbar.css";  // Link to your CSS file
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">RoomEase</div>
+      <div className="navbar-logo">RoomEaze</div>
       <ul className="navbar-links">
         <li><Link to="/profile">Profile</Link></li>
         <li><Link to="/bulletin">Bulletin</Link></li>
@@ -12,7 +12,7 @@ function Navbar() {
         <li><Link to="/calender/scheduler">Schedule</Link></li>
         <li><Link to="/todolist/todo">To-Do List</Link></li>
         <li><Link to="/home">Home</Link></li>
-        <li><Link to="/Users/lauragonzalez/roomeaze/roomeaze/src/pages/landing-page/landing-page.jsx">Sign out</Link></li>
+        <li><Link to="/src/pages/landing-page/landing-page.jsx">Sign out</Link></li>
       </ul>
     </nav>
   );
