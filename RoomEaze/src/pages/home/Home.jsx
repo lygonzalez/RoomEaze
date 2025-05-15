e0eeeff99cbdc4b7d163a206e8bb8e869725700a
Laura <lygg0589@gmail.com>
2025-05-14 18:27:43 -0700

    icons

--- RoomEaze/src/pages/home/Home.jsx
+++ RoomEaze/src/pages/home/Home.jsx
@@ -7,14 +7,15 @@ function Home() {
     <div className="home-container">
       <div className="home-box">
         <Link to="/calender/scheduler" className="home-button">
+        <img src="/calender_icon.png" alt="Task Icon" className="icon" />
           Calendar View
         </Link>
         <Link to="/todolist/todo" className="home-button">
-          <img src="/notebook-icon.png" alt="Task Icon" className="icon" />
+          <img src="/todoicon.png" alt="Task Icon" className="icon" />
           Task-list View
         </Link>
         <Link to="/bulletin" className="home-button">
-          <img src="/chat-icon.png" alt="Message Icon" className="icon" />
+          <img src="/bulletin.png" alt="Message Icon" className="icon" />
           Message Board
         </Link>
       </div>
